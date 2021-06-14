4d51162fed28bd639a84d9f8a82625ae53c4da6d
empalor <empalor@github.com>
2021-06-14 23:03:52 +0300

    #2

--- app/src/main/java/com/example/giftcardsfun/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/giftcardsfun/viewmodel/MainViewModel.kt
@@ -1,11 +1,18 @@
 package com.example.giftcardsfun.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.giftcardsfun.repository.GiftCardsRepo
+import com.example.giftcardsfun.view.MainFragment
 
 class MainViewModel : ViewModel() {
+    private lateinit var stateLiveData: MutableLiveData<MainFragment.State>
 
-    fun refresh() {
+    fun refresh() = try {
+
         GiftCardsRepo.refresh()
+
+    } catch (e: Exception) {
+        stateLiveData.postValue(MainFragment.State.failure)
     }
 }
