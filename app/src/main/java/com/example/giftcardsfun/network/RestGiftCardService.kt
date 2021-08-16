6231fe799b73d94a2cad020e7b64e73136e87a77
aviv <avivmaor9@gmail.com>
2021-08-16 21:18:52 +0300

    add firebase code - beginning

--- app/src/main/java/com/example/giftcardsfun/network/RestGiftCardService.kt
+++ app/src/main/java/com/example/giftcardsfun/network/RestGiftCardService.kt
@@ -1,12 +1,37 @@
 package com.example.giftcardsfun.network
 
+import android.util.Log
+import com.google.firebase.database.*
 import kotlinx.coroutines.delay
 
 class RestGiftCardService {
+    var firebaseDatabase: FirebaseDatabase = FirebaseDatabase.getInstance()
+
     suspend fun getStores(): GiftCardServer {
+        printAllFirebaseDB()
         //Mock
         delay(1500)
         val stores: List<String> = listOf("Zara", "Fox")
         return GiftCardServer(stores, "MAX")
+    }
+
+    fun printAllFirebaseDB() {
+        val dbRef = firebaseDatabase.reference
+        dbRef.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    Log.d("CARD", "DB state:")
+                    for (d in dataSnapshot.children) {
+                        val key = d.key
+                        val obj = d.value
+                        Log.d("CARD", "($key : $obj)")
+                    }
+                }
+            } //onDataChange
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.d("CARD", "onCancelled")
+            } //onCancelled
+        })
     }
 }
